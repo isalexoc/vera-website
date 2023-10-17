d91bf20a226e27fdf0959bde5f960d73bb47e114
Isaac Orraiz <isaacorraiz@gmail.com>
2023-10-17 17:36:15 -0400

    Header and form ready

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Replace text in header
+
+const checkReplace = document.querySelector(".replace-me");
+
+if (checkReplace != null) {
+  const replace = new ReplaceMe(checkReplace, {
+    animation: "animated fadeIn",
+    speed: 2000,
+    separator: ",",
+    loopCount: "infinite",
+    autoRun: true,
+  });
+}
